41741aec91940df8de12bd35998b02d6d0c90a77
Yuki Okubo <y-okubo@nekojarashi.com>
2018-04-09 16:32:07 +0900

    Refactor

--- aws-multipart-upload.go
+++ aws-multipart-upload.go
@@ -11,10 +11,10 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/cenkalti/backoff"
 	humanize "github.com/dustin/go-humanize"
 	"github.com/joho/godotenv"
 	"golang.org/x/sync/errgroup"
@@ -22,15 +22,21 @@ import (
 
 // Part defines part information
 type Part struct {
-	number int
-	buffer []byte
+	number  int
+	content []byte
 }
 
 const (
-	maxPartSize = int64(5242880) // S3 Minimum
+	maxPartSize = int64(5242880) // S3 Minimum (5MB)
 	maxRetries  = 3
 	maxWorkers  = 10
 )
+
+var awsAccessKeyID string
+var awsSecretAccessKey string
+var awsBucketRegion string
+var awsBucketName string
+var awsEndpoint string
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU() / 2) // http://ascii.jp/elem/000/001/480/1480872/
@@ -42,16 +48,18 @@ func main() {
 		log.Fatal("Error loading .aws.config file")
 	}
 
-	awsAccessKeyID := os.Getenv("S3_ACCESS_KEY_ID")
-	awsSecretAccessKey := os.Getenv("S3_SECRET_ACCESS_KEY")
-	awsBucketRegion := os.Getenv("S3_BUCKET_REGION")
-	awsBucketName := os.Getenv("S3_BUCKET_NAME")
-	awsEndpoint := os.Getenv("S3_ENDPOINT")
+	awsAccessKeyID = os.Getenv("S3_ACCESS_KEY_ID")
+	awsSecretAccessKey = os.Getenv("S3_SECRET_ACCESS_KEY")
+	awsBucketRegion = os.Getenv("S3_BUCKET_REGION")
+	awsBucketName = os.Getenv("S3_BUCKET_NAME")
+	awsEndpoint = os.Getenv("S3_ENDPOINT")
 
-	path := os.Args[1]
+	upload(os.Args[1])
+}
 
+func upload(path string) {
 	creds := credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, "")
-	_, err = creds.Get()
+	_, err := creds.Get()
 	if err != nil {
 		log.Println("Bad credentials:", err)
 	}
@@ -67,16 +75,15 @@ func main() {
 
 	stat, _ := f.Stat()
 	size := stat.Size()
-	buf := make([]byte, size)
-	ct := http.DetectContentType(buf)
-	f.Read(buf)
+	content := make([]byte, size)
+	f.Read(content)
 
 	log.Printf("Upload size is %s\n", humanize.Bytes(uint64(size)))
 
 	input := &s3.CreateMultipartUploadInput{
 		Bucket:      aws.String(awsBucketName),
 		Key:         aws.String(path),
-		ContentType: aws.String(ct),
+		ContentType: aws.String(http.DetectContentType(content)),
 	}
 
 	sTime := time.Now()
@@ -89,7 +96,7 @@ func main() {
 
 	log.Println("Created multipart upload request")
 
-	parts := divideFileIntoParts(buf)
+	parts := divideFileIntoParts(content)
 	length := len(parts)
 
 	q := make(chan Part, length)
@@ -100,19 +107,21 @@ func main() {
 
 	var completedParts = make([]*s3.CompletedPart, length)
 
-	// Init workers
+	// Booting workers
 	for i := 0; i < maxWorkers; i++ {
 		eg.Go(func() error {
+			awsctx := aws.BackgroundContext()
 			for {
 				select {
 				case <-ctx.Done():
+					log.Println("Worker canceled")
 					return fmt.Errorf("Worker canceled")
 				default:
 					part, ok := <-q
 					if !ok {
 						return nil
 					}
-					completedPart, err := uploadPart(svc, resp, part.buffer, part.number)
+					completedPart, err := uploadPart(awsctx, svc, resp, part.content, part.number)
 					if err != nil {
 						log.Println(err.Error())
 						err := abortMultipartUpload(svc, resp)
@@ -134,6 +143,11 @@ func main() {
 
 	close(q)
 
+	// go func() {
+	// 	time.Sleep(8 * time.Second)
+	// 	cancel()
+	// }()
+
 	if err := eg.Wait(); err != nil {
 		log.Println(err)
 		cancel()
@@ -141,11 +155,6 @@ func main() {
 	}
 
 	cancel()
-
-	// PartNumber で並び替えしていないとエラーになるので対処
-	// sort.Slice(completedParts, func(i, j int) bool {
-	// 	return *completedParts[i].PartNumber < *completedParts[j].PartNumber
-	// })
 
 	completeResponse, err := completeMultipartUpload(svc, resp, completedParts)
 	if err != nil {
@@ -155,13 +164,13 @@ func main() {
 
 	log.Println("Successfully uploaded file:", completeResponse.String())
 
-	dTime := time.Now()
-	duration := dTime.Sub(sTime)
+	eTime := time.Now()
+	duration := eTime.Sub(sTime)
 	log.Println("Elapsed time:", duration)
 }
 
-func divideFileIntoParts(buf []byte) []Part {
-	contentSize := int64(len(buf))
+func divideFileIntoParts(content []byte) []Part {
+	contentSize := int64(len(content))
 
 	size := contentSize / maxPartSize
 	if contentSize%maxPartSize != 0 {
@@ -181,8 +190,8 @@ func divideFileIntoParts(buf []byte) []Part {
 		}
 
 		parts[number-1] = Part{
-			number: number,
-			buffer: buf[current : current+length],
+			number:  number,
+			content: content[current : current+length],
 		}
 
 		remaining -= length
@@ -194,20 +203,7 @@ func divideFileIntoParts(buf []byte) []Part {
 	return parts
 }
 
-func completeMultipartUpload(svc *s3.S3, resp *s3.CreateMultipartUploadOutput, completedParts []*s3.CompletedPart) (*s3.CompleteMultipartUploadOutput, error) {
-	completeInput := &s3.CompleteMultipartUploadInput{
-		Bucket:   resp.Bucket,
-		Key:      resp.Key,
-		UploadId: resp.UploadId,
-		MultipartUpload: &s3.CompletedMultipartUpload{
-			Parts: completedParts,
-		},
-	}
-	return svc.CompleteMultipartUpload(completeInput)
-}
-
-func uploadPart(svc *s3.S3, resp *s3.CreateMultipartUploadOutput, fileBytes []byte, partNumber int) (*s3.CompletedPart, error) {
-	tryNum := 1
+func uploadPart(ctx aws.Context, svc *s3.S3, resp *s3.CreateMultipartUploadOutput, fileBytes []byte, partNumber int) (*s3.CompletedPart, error) {
 	partInput := &s3.UploadPartInput{
 		Body:          bytes.NewReader(fileBytes),
 		Bucket:        resp.Bucket,
@@ -217,35 +213,61 @@ func uploadPart(svc *s3.S3, resp *s3.CreateMultipartUploadOutput, fileBytes []by
 		ContentLength: aws.Int64(int64(len(fileBytes))),
 	}
 
-	for tryNum <= maxRetries {
-		uploadResult, err := svc.UploadPart(partInput)
-		if err != nil {
-			if tryNum == maxRetries {
-				if aerr, ok := err.(awserr.Error); ok {
-					return nil, aerr
-				}
-				return nil, err
-			}
-			log.Printf("Retrying to upload part #%v\n", partNumber)
-			tryNum++
-		} else {
-			log.Printf("Uploaded part #%v\n", partNumber)
-			return &s3.CompletedPart{
-				ETag:       uploadResult.ETag,
-				PartNumber: aws.Int64(int64(partNumber)),
-			}, nil
-		}
+	var partOutput *s3.UploadPartOutput
+	operation := func() error {
+		var err error
+		partOutput, err = svc.UploadPartWithContext(ctx, partInput)
+		return err
 	}
-	return nil, nil
+
+	notify := func(err error, duration time.Duration) {
+		log.Printf("%v %v\n", duration, err)
+	}
+
+	// err := backoff.Retry(operation, backoff.WithMaxRetries(backoff.NewExponentialBackOff(), maxRetries))
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return nil, err
+	// }
+
+	err := backoff.RetryNotify(
+		operation,
+		backoff.WithMaxRetries(backoff.NewExponentialBackOff(), maxRetries),
+		notify)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	log.Printf("Uploaded part #%v\n", partNumber)
+
+	return &s3.CompletedPart{
+		ETag:       partOutput.ETag,
+		PartNumber: aws.Int64(int64(partNumber)),
+	}, nil
+}
+
+func completeMultipartUpload(svc *s3.S3, resp *s3.CreateMultipartUploadOutput, completedParts []*s3.CompletedPart) (*s3.CompleteMultipartUploadOutput, error) {
+	input := &s3.CompleteMultipartUploadInput{
+		Bucket:   resp.Bucket,
+		Key:      resp.Key,
+		UploadId: resp.UploadId,
+		MultipartUpload: &s3.CompletedMultipartUpload{
+			Parts: completedParts,
+		},
+	}
+	awsctx := aws.BackgroundContext()
+	return svc.CompleteMultipartUploadWithContext(awsctx, input)
 }
 
 func abortMultipartUpload(svc *s3.S3, resp *s3.CreateMultipartUploadOutput) error {
 	log.Println("Aborting multipart upload for UploadId#" + *resp.UploadId)
-	abortInput := &s3.AbortMultipartUploadInput{
+	input := &s3.AbortMultipartUploadInput{
 		Bucket:   resp.Bucket,
 		Key:      resp.Key,
 		UploadId: resp.UploadId,
 	}
-	_, err := svc.AbortMultipartUpload(abortInput)
+	awsctx := aws.BackgroundContext()
+	_, err := svc.AbortMultipartUploadWithContext(awsctx, input)
 	return err
 }
